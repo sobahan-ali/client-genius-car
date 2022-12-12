e9b76782e60504933f27f85704851c9e374825d9
sobahan-ali <sobahaan.ali@gmail.com>
2022-12-12 09:49:47 +0600

    more practice genius car project

---
+++ src/Pages/Home/Home/Banner/Banner.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import img1 from '../../../../assets/images/banner/1.jpg'
+import img2 from '../../../../assets/images/banner/2.jpg'
+import img3 from '../../../../assets/images/banner/3.jpg'
+import img4 from '../../../../assets/images/banner/4.jpg'
+import img5 from '../../../../assets/images/banner/5.jpg'
+import img6 from '../../../../assets/images/banner/6.jpg'
+import BannerItem from './BannerItem';
+import './Banner.css';
+
+const Banner = () => {
+
+    const slides = [
+        {
+            prv: 6,
+            id: 1,
+            next: 2,
+            image: img1,
+        },
+        {
+            prv: 1,
+            id: 2,
+            next: 3,
+            image: img2,
+        },
+        {
+            prv: 2,
+            id: 3,
+            next: 4,
+            image: img3,
+        },
+        {
+            prv: 3,
+            id: 4,
+            next: 5,
+            image: img4,
+        },
+        {
+            prv: 4,
+            id: 5,
+            next: 6,
+            image: img5,
+        },
+        {
+            prv: 5,
+            id: 6,
+            next: 1,
+            image: img6,
+        },
+    ]
+    return (
+        <div className="carousel w-full">
+            {
+                slides.map(slide => <BannerItem
+                    key={slide.id}
+                    slide={slide}
+                ></BannerItem>)
+            }
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/Pages/Home/Home/Banner/BannerItem.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+
+const BannerItem = ({ slide }) => {
+    const { image, id, prv, next } = slide;
+    return (
+        <div id={`slide${id}`} className="carousel-item relative w-full">
+            <div className='carousel-img'> <img src={image} className="w-full object-fill" alt="" /></div>
+
+            <div className="absolute flex justify-end transform -translate-y-1/2 left-24 top-1/4">
+                <h1 className='font-bold text-7xl text-white'>Affordable <br /> Price For Car <br /> Servicing</h1>
+            </div>
+            <div className="absolute flex justify-end transform -translate-y-1/2 left-24 top-1/2">
+                <p className='text-xl text-white'>There are many variations of passages of  available, but <br /> the majority have suffered alteration in some form</p>
+            </div>
+            <div className="absolute flex justify-end transform -translate-y-1/2 left-24 top-3/4">
+                <button className="btn btn-active btn-secondary mr-5">Discover more</button>
+                <button className="btn btn-outline btn-warning">Latest Project</button>
+            </div>
+            <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-1">
+                <a href={`#slide${prv}`} className="btn btn-circle mr-5">❮</a>
+                <a href={`#slide${next}`} className="btn btn-circle">❯</a>
+            </div>
+        </div>
+    );
+};
+
+export default BannerItem;
